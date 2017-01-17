723a671c9d4e0be8b9c7a1d5bb0c914b1f01d18f
Jenny Lai <jenny.lai@flatironschool.com>
2017-01-17 17:41:03 -0500

    Done.

---
+++ index.js
@@ -0,0 +1,28 @@
+
+function theTruth() {
+  return true
+}
+
+function sayHey() {
+  return 'Hey!'
+}
+
+
+function sayHeyFriend(name) {
+  return `Hey, ${name}!`
+}
+
+
+function favoriteIceCream(icecream) {
+  return `I love ${icecream}`
+}
+
+
+function shouting(string) {
+  return string.toUpperCase()
+}
+
+
+function roundDown(number) {
+  return parseInt(number, 0)
+}
